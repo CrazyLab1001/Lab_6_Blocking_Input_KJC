877173dcea260f852c18439576d2b5e37866f6ea
CrazyLab1001 <cramerkj@mail.uc.edu>
2025-02-16 16:10:28 -0500

    Task 1 complete!

--- src/CtoFConverter.java
+++ src/CtoFConverter.java
@@ -1,5 +1,23 @@
+import java.util.Scanner;
+
 public class CtoFConverter {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        double tempC = -1;
+        double tempF = 0;
+        String trash = "";
+        do {
+            System.out.println("What is the temperature (in Celsius)? ");
+            Scanner in = new Scanner(System.in); // input
+            if (in.hasNextDouble()){
+                tempC = in.nextDouble();
+                in.nextLine();
+                tempF = (tempC * ((double) 9 /5) + 32);
+                System.out.println("The temperature in Fahrenheit is " + tempF + " degrees!");
+
+            } else {
+                trash = in.nextLine();
+                System.out.println("Invalid input. " + trash + ". Try entering a number!");
+            }
+        } while (tempC == -1);
     }
 }
